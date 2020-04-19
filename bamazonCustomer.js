3360f6e0c65f3c246ab793b7c982afd5984cb2c4
meredithparks <mereditheparks@gmail.com>
2020-04-18 21:51:14 -0600

    my sql connection on js file

---
+++ bamazonCustomer.js
@@ -0,0 +1,14 @@
+var inquirer = require('inquirer');
+var mysql = require('mysql');
+
+var connection = my.sql.createConnection({
+    host:'localhost',
+    port: 3306,
+
+    user:'root',
+
+    password:'Duchess143!',
+    database: 'bamazon_db'
+
+
+});
