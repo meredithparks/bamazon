92300b815d652afa7a8ed0e0cd646929ef4dcdc7
meredithparks <mereditheparks@gmail.com>
2020-04-18 22:04:19 -0600

    connection finally worked to mysql

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,14 +1,19 @@
 var inquirer = require('inquirer');
 var mysql = require('mysql');
 
-var connection = my.sql.createConnection({
-    host:'localhost',
+var connection = mysql.createConnection({
+    host:"localhost",
     port: 3306,
 
-    user:'root',
+    user:"root",
 
     password:'Duchess143!',
     database: 'bamazon_db'
 
 
 });
+
+connection.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
